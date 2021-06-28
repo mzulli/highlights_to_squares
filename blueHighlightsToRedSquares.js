33ac446b95af2304ee79f002397aef2a57a2960f
Michelle Z <mzulli@users.noreply.github.com>
2021-06-28 17:50:42 -0400

    Update

--- blueHighlightsToRedSquares.js
+++ blueHighlightsToRedSquares.js
@@ -4,7 +4,21 @@ var oDoc = event.target;
 var aAnnts = oDoc.getAnnots();
 
 for (var i = 0; i < aAnnts.length; i++) {
-    if (aAnnts[i].type == "Highlight" && color.equal(aAnnts[i].strokeColor, color.cyan)) {
+    if (aAnnts[i].type == "Highlight") {
+        applicationCache.alert({
+            cMsg: "Is this the color to convert to squares?"
+            cTitle: "Question"
+            nType: 3
+        });
+        if (nButton == "Yes") {
+            var selectedColor = this.strokeColor;
+            break;
+        }
+    }
+}
+
+for (var i = 0; i < aAnnts.length; i++) {
+    if (aAnnts[i].type == "Highlight" && color.equal(aAnnts[i].strokeColor, selectedColor)) {
         aAnnts[i].type = "Square";
         aAnnts[i].style = "S";
         aAnnts[i].fillColor = color.transparent;
